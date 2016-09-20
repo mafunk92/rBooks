420bd7c11cebe67fea3dc3478ca5cf187b1de2e8
Michael Funk <mafunk92@gmail.com>
2016-09-19 20:44:19 -0500

    initial commit

---
+++ app/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return[
+        { title: 'Java'},
+        { title: 'Dark Tower'},
+        { title: 'Fun Facts'},
+        { title: 'Call to Arms'},
+        { title: 'Halo'}
+    ]
+}
